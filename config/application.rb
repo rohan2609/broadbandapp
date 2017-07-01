60ac938be3163d5eb49800340203a562cdf3d1b0
Rohan <routrohan90@gmail.com>
2017-07-01 13:44:58 +0530

    Added css

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Broadbandapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    # Precompile *all* assets, except those that start with underscore
+config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
